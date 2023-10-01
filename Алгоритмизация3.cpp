3fd34bfe050178df5e4da689eede9469a54e55cb
bebebrrr <kubanova04@gmail.com>
2023-10-01 19:08:41 +0300

    Программа

---
+++ Алгоритмизация3.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int f(int a[], int n, int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == x)
+        {
+            return i;
+        }
+    }
+       return -1;
+}
+
+int main()
+{
+    int const n = 100;
+    int a[n];
+    int x;
+    clock_t now = clock();
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 10;
+        
+    }
+    cout << endl << "x=";
+    x = 10;
+    int result = f(a, n, x);
+    if (result == -1)
+    {
+        cout << "Element not found" << endl;
+    }
+    else
+    {
+        cout << "Element found at index " << result << endl;
+    }
+    clock_t end = clock();
+    double seconds = (double)(end - now) / CLOCKS_PER_SEC;
+    cout << endl << seconds << endl;
+    return 0;
+}
